08fba7d1886dabba9f4ab7f084c35049797053f4
Peter Luladjiev <luladjiev@gmail.com>
2019-10-13 11:27:11 +0300

    Add enemy and detect it

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,7 +2,11 @@ extends Node2D
 
 signal hit
 
-const actions = {
+enum ACTIONS {
+	MOVE, ATTACK
+}
+
+const inputs = {
 	"left": Vector2.LEFT,
 	"right": Vector2.RIGHT,
 	"up": Vector2.UP,
@@ -15,23 +19,41 @@ onready var ray_cast = $RayCast2D
 func _ready():
 #	Snap player to the grid
 	position = position.snapped(Vector2.ONE * TILE_SIZE)
-	position += Vector2.ONE * TILE_SIZE/2		
+	position += Vector2.ONE * TILE_SIZE/2
 
 
 func _unhandled_input(event):
-	for direction in actions.keys():
+	for direction in inputs.keys():
 		if event.is_action_pressed(direction):
-			move(direction)
+			do_action(direction)
 
 
 func _on_Player_hit():
-	pass
+	emit_signal("hit")
 
 
-func move(index):
-	var direction = actions[index]
+func do_action(index):
+	var direction = inputs[index]
+
+	match(check_tile(direction)):
+		ACTIONS.MOVE:
+			position += direction * TILE_SIZE
+
+
+func check_tile(direction):
 	ray_cast.cast_to = direction * TILE_SIZE
 	ray_cast.force_raycast_update()
-	if (!ray_cast.is_colliding()):
-		position += actions[index] * TILE_SIZE
 
+	if !ray_cast.is_colliding():
+		return ACTIONS.MOVE
+
+	var collider = ray_cast.get_collider()
+
+#	If there is no owner that means that we hit an obstacle
+	if !collider.owner:
+		return
+
+	var owner = collider.owner
+
+	if owner.is_in_group(Groups.ENEMY):
+		print("enemy hit")
